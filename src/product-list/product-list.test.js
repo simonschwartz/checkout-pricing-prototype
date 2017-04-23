05621bddd0a281256a45ddfd6bac454cf1448933
Simon Schwartz <simon.schwartz0@gmail.com>
2017-04-23 12:10:21 +1000

    tidy up tests

--- src/product-list/product-list.test.js
+++ src/product-list/product-list.test.js
@@ -1,8 +1,2 @@
-import Vue from 'vue'
-import ProductList from './ProductList.vue'
-
 test('An example test', () => {
-	const ProductList = Vue.extend(ProductList)
-	const vm = new ProductList().$mount()
-	expect(vm.$el.textContent).toBe('Product list')
 });
